b37ace88ef997c268121640ac32db3c32900dfcd
Dennis Becker <mail.dennisbecker@gmail.com>
2014-03-17 10:24:06 +0100

    adds more tasks to Gruntfile.js with required dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,77 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        phplint: {
+            all: ['src/**/*.php', 'tests/**/*.php']
+        },
         phpunit: {
-            classes: {
-                dir: 'tests/'
+            fast: {
+                dir: 'tests',
+                options: {
+                    bin: 'vendor/bin/phpunit',
+                    noConfiguration: true,
+                    bootstrap: 'vendor/autoload.php',
+                    colors: true,
+                    strict: true
+                }
+            },
+            coverage: {
+                dir: 'tests',
+                options: {
+                    bin: 'vendor/bin/phpunit',
+                    noConfiguration: true,
+                    bootstrap: 'vendor/autoload.php',
+                    coverage: true,
+                    colors: true,
+                    strict: true
+                }
+            },
+            full: {
+                classes: {
+                    dir: 'tests/'
+                },
+                options: {
+                    bin: 'vendor/bin/phpunit',
+                    bootstrap: 'vendor/autoload.php',
+                    colors: true,
+                    strict: true
+                }
+            }
+        },
+        phpcs: {
+            application: {
+                dir: ['src', 'tests']
             },
             options: {
-                bin: 'vendor/bin/phpunit',
-                bootstrap: 'vendor/autoload.php',
-                coverage: true,
-                colors: true,
-                strict: true
+                bin: 'vendor/bin/phpcs',
+                standard: 'PSR1'
+            }
+        },
+        phpmd: {
+            application: {
+                dir: ['src']
+            },
+            options: {
+                bin: 'vendor/bin/phpmd',
+                rulesets: 'codesize,naming,unusedcode'
             }
         },
         watch: {
             test: {
                 files: ['src/**/*.php', 'tests/**/*Test.php'],
-                tasks: ['phpunit']
+                tasks: ['phpunit:fast']
+            }
+        },
+        githooks: {
+            all: {
+                'pre-commit': 'phplint phpunit:fast'
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-githooks');
+    grunt.loadNpmTasks('grunt-phplint');
     grunt.loadNpmTasks('grunt-phpunit');
+    grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks('grunt-phpmd');
 };
