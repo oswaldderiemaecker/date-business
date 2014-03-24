7a8b5ed783528b0925a2bd5385e8e9727b38f0ea
Dennis Becker <mail.dennisbecker@gmail.com>
2014-03-24 14:43:46 +0100

    implements addBusinessDays, subBusinessDays, nextBusinessDay and previousBusinessDay to DateBusiness

--- src/DateBusiness.php
+++ src/DateBusiness.php
@@ -2,19 +2,149 @@
 
 namespace Intl;
 
+use DateInterval;
 use DateTime;
+use UnexpectedValueException;
 
 class DateBusiness extends DateTime
 {
+    /**
+     * @var array
+     */
+    private $staticHolidays = array();
+
+    /**
+     * @var array
+     */
+    private $variableHolidays = array();
+
     /**
      * @return bool
      */
     public function isBusinessDay()
     {
-        if ($this->format('N') < 6) {
+        if ($this->format('N') > 5) {
+            return false;
+        }
+
+        if ($this->isHoliday()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isHoliday()
+    {
+        if (in_array($this->format('m-d'), $this->staticHolidays)) {
+            return true;
+        }
+
+        if (in_array($this->format('Y-m-d'), $this->variableHolidays)) {
             return true;
         }
 
         return false;
+    }
+
+    /**
+     * Adds an amount of business days to a DateBusiness object
+     * @param int $days
+     * @throws UnexpectedValueException
+     * @return DateBusiness
+     */
+    public function addBusinessDay($days)
+    {
+        $this->validateDays($days);
+
+        $i = 0;
+        $targetDate = clone $this;
+        $interval = new DateInterval('P1D');
+
+        while ($i < $days) {
+            $targetDate->add($interval);
+
+            if (!$targetDate->isBusinessDay()) {
+                ++$days;
+            }
+            ++$i;
+        }
+
+        return $targetDate;
+    }
+
+    /**
+     * Adds an amount of business days to a DateBusiness object
+     * @param int $days
+     * @throws UnexpectedValueException
+     * @return DateBusiness
+     */
+    public function subBusinessDays($days)
+    {
+        $this->validateDays($days);
+
+        $i = 0;
+        $targetDate = clone $this;
+        $interval = new DateInterval('P1D');
+
+        while ($i < $days) {
+            $targetDate->sub($interval);
+
+            if (!$targetDate->isBusinessDay()) {
+                ++$days;
+            }
+
+            ++$i;
+        }
+
+        return $targetDate;
+    }
+
+    /**
+     * @throws UnexpectedValueException
+     * @return DateBusiness
+     */
+    public function nextBusinessDay()
+    {
+        return $this->addBusinessDay(1);
+    }
+
+    /**
+     * @throws UnexpectedValueException
+     * @return DateBusiness
+     */
+    public function previousBusinessDay()
+    {
+        return $this->subBusinessDays(1);
+    }
+
+    /**
+     * @param array $variableHolidays
+     */
+    public function setVariableHolidays(array $variableHolidays)
+    {
+        $this->variableHolidays = $variableHolidays;
+    }
+
+    /**
+     * @param array $staticHolidays
+     */
+    public function setStaticHolidays(array $staticHolidays)
+    {
+        $this->staticHolidays = $staticHolidays;
+    }
+
+    /**
+     * @param int $days
+     * @throws UnexpectedValueException
+     */
+    private function validateDays($days)
+    {
+        if (!is_int($days)) {
+            throw new UnexpectedValueException('Parameter "days" must be of type integer.');
+        }
     }
 }
--- tests/DateBusinessTest.php
+++ tests/DateBusinessTest.php
@@ -11,7 +11,19 @@ class DateBusinessTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        error_reporting(-1);
         $this->dateBusiness = new DateBusiness();
+        $this->dateBusiness->setStaticHolidays(array(
+            '01-01',
+            '05-01',
+            '12-25',
+            '12-26',
+        ));
+        $this->dateBusiness->setVariableHolidays(array(
+            '2014-04-18',
+            '2014-04-20',
+            '2014-04-21',
+        ));
     }
 
     public function testDateBusinessExtendsDateTime()
@@ -24,15 +36,87 @@ class DateBusinessTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(date('Y-m-d'), $this->dateBusiness->format('Y-m-d'));
     }
 
-    public function testIsValidBusinessDayOnMonday()
+    public function testIsBusinessDayReturnsTrueOnMonday()
     {
         $this->dateBusiness->setDate(2014, 3, 10);
         $this->assertTrue($this->dateBusiness->isBusinessDay());
     }
 
-    public function testIsValidBusinessDayOnSaturday()
+    public function testIsBusinessDayReturnsFalseOnSaturday()
     {
         $this->dateBusiness->setDate(2014, 3, 15);
         $this->assertFalse($this->dateBusiness->isBusinessDay());
+    }
+
+    public function testIsBusinessDayReturnsFalseOnTargetHoliday()
+    {
+        $this->dateBusiness->setDate(2014, 1, 1); // Wednesday
+        $this->assertFalse($this->dateBusiness->isBusinessDay());
+    }
+
+    public function testIsHolidayReturnsTrueOnNewYear()
+    {
+        $this->dateBusiness->setDate(2014, 1, 1);
+        $this->assertTrue($this->dateBusiness->isHoliday());
+    }
+
+    public function testIsHolidayReturnsFalseOnTheDayAfterNewYear()
+    {
+        $this->dateBusiness->setDate(2014, 1, 2);
+        $this->assertFalse($this->dateBusiness->isHoliday());
+    }
+
+    public function testIsHolidayReturnsTrueOnVariableHoliday()
+    {
+        $this->dateBusiness->setDate(2014, 4, 18);
+        $this->assertTrue($this->dateBusiness->isHoliday());
+    }
+
+    public function testAddOneDayReturnsNextBusinessDay()
+    {
+        $this->dateBusiness->setDate(2014, 1, 2);
+        $targetDate = $this->dateBusiness->addBusinessDay(1);
+
+        $this->assertSame('2014-01-03', $targetDate->format('Y-m-d'));
+    }
+
+    public function testAddWorksOverEasternHolidays()
+    {
+        $this->dateBusiness->setDate(2014, 4, 18);
+        $targetDate = $this->dateBusiness->addBusinessDay(2);
+
+        $this->assertSame('2014-04-23', $targetDate->format('Y-m-d'));
+    }
+
+    public function testNextAddsOneBusinessDay()
+    {
+        $this->dateBusiness->setDate(2014, 4, 17);
+        $targetDate = $this->dateBusiness->nextBusinessDay();
+
+        $this->assertSame('2014-04-22', $targetDate->format('Y-m-d'));
+    }
+
+    public function testSubOneDayReturnsPreviousBusinessDay()
+    {
+        $this->dateBusiness->setDate(2014, 1, 3);
+        $targetDate = $this->dateBusiness->subBusinessDays(1);
+
+        $this->assertSame('2014-01-02', $targetDate->format('Y-m-d'));
+    }
+
+    public function testSubWorksOverEasternHolidays()
+    {
+        $this->dateBusiness->setDate(2014, 4, 23);
+        $targetDate = $this->dateBusiness->subBusinessDays(2);
+
+        $this->assertSame('2014-04-17', $targetDate->format('Y-m-d'));
+    }
+
+    public function testPrevSubstractsOneBusinessDay()
+    {
+        $this->dateBusiness->setDate(2014, 4, 22);
+        $targetDate = $this->dateBusiness->previousBusinessDay();
+
+        $this->assertSame('2014-04-17', $targetDate->format('Y-m-d'));
     }
 }
